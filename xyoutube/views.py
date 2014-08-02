5958f0895540ebde330aa1bb6bfa8d31b0f1a711
Alex Schokking <aschokking@gmail.com>
2014-08-01 17:04:04 -0700

    starting on view

--- xyoutube/views.py
+++ xyoutube/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from apiclient.discovery import build
+
+def get_api():
+	key = 'AIzaSyDMg-W4M-P8wGRZrEPM4h67xbIJojGRpj8'
+	api = build("youtube", "v3", developerKey=key)
+	return api
+
 # Create your views here.
+def recent_videos(request):
+	api = get_api()
+
+	teamxbot_channel_id = 'UCaWKhXUXqBeQHjnp4HhpOxg'
+	results = api.search().list(part="id,snippet", channelId=teamxbot_channel_id).execute()
+	
